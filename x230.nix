24491da6668ec35ad74c926633677b87500c27be
David Wagner <wagdav@gmail.com>
2020-04-22 21:45:31 +0200

    Remove superfluous statement
    By default xterm.enable is false, so this statement has no effect.

--- x230.nix
+++ x230.nix
@@ -95,8 +95,6 @@
         enableContribAndExtras = true;
       };
     };
-
-    desktopManager.xterm.enable = false;
   };
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
