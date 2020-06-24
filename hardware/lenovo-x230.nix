e2d6a085b3f65a151b6ef45b7327103d333716e4
David Wagner <wagdav@gmail.com>
2020-06-24 13:29:49 +0200

    Include X230 specific tuning from nixos-hardware.

---
+++ hardware/lenovo-x230.nix
@@ -0,0 +1,11 @@
+let
+  revision = "a78b8b647692919fe871f5726b7b5e01dd725a88";
+  sha256 = "0lcfj18wbi4a7nv6fc1pvq28m210laviy00d4dngklrppihykj40";
+
+  nixos-hardware = builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixos-hardware/archive/${revision}.tar.gz";
+    inherit sha256;
+  };
+
+in (import "${nixos-hardware}/lenovo/thinkpad/x230")
+
--- x230.nix
+++ x230.nix
@@ -6,7 +6,8 @@
 
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [ ./hardware/lenovo-x230.nix
+      # Include the results of the hardware scan.
       /etc/nixos/hardware-configuration.nix
     ];
 
