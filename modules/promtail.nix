470e905f9af019389ada642ffcb98e0b0710e7bd
David Wagner <wagdav@gmail.com>
2020-07-21 20:47:51 +0200

    Add Promtail module

---
+++ modules/promtail.nix
@@ -0,0 +1,66 @@
+{ config, pkgs, ... }:
+
+let
+
+  httpPort = 9080;
+
+  configFile = pkgs.writeText "promtail-config.yaml" ''
+    server:
+      http_listen_port: ${toString httpPort}
+      grpc_listen_port: 0
+
+    positions:
+      filename: /tmp/positions.yaml
+
+    clients:
+      - url: http://nuc.thewagner.home:3100/loki/api/v1/push
+
+    scrape_configs:
+    - job_name: journal
+      journal:
+        path: /var/log/journal
+        max_age: 12h
+        labels:
+          job: systemd-journal
+      relabel_configs:
+       - source_labels: ['__journal__systemd_unit' ]
+         target_label: 'unit'
+       - source_labels: ['__journal__hostname']
+         target_label: 'hostname'
+  '';
+
+in
+
+{
+  imports = [ ./consul-catalog.nix ];
+
+  systemd.services.promtail = {
+    description = "Promtail service for Loki";
+    wantedBy = [ "multi-user.target" ];
+
+    serviceConfig = {
+      ExecStart = ''
+        ${pkgs.grafana-loki}/bin/promtail --config.file ${configFile}
+      '';
+      User = "promtail";
+      Group = "promtail";
+    };
+  };
+
+  users.groups.promtail = { };
+  users.users.promtail = {
+    description = "Promtail Service User";
+    group = "promtail";
+    extraGroups = [ "systemd-journal" ];
+    isSystemUser = true;
+  };
+
+  services.consul.catalog = [
+    {
+      name = "promtail";
+      port = httpPort;
+    }
+  ];
+
+  networking.firewall.allowedTCPPorts = [ httpPort ];
+}
