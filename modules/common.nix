24b92475992df482b952f3c48166d33837569843
David Wagner <wagdav@gmail.com>
2021-02-20 14:21:37 +0100

    Tune gc options

--- modules/common.nix
+++ modules/common.nix
@@ -1,9 +1,9 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 
 {
   nix.gc = {
     automatic = true;
-    options = "--delete-older-than 30d";
+    options = ''--max-freed "$((10 * 1024**3 - 1024 * $(df -P -k /nix/store | tail -n 1 | ${pkgs.gawk}/bin/awk '{ print $4 }')))"'';
   };
 
   i18n.defaultLocale = "en_US.UTF-8";
