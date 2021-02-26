33c8ed18b78f7f80f95ebf25ae683184ef322abe
David Wagner <wagdav@gmail.com>
2021-02-26 19:32:50 +0100

    Disable intel_ips to prevent WiFi connectivity drop
    Source: https://unix.stackexchange.com/questions/555784/iwlwifi-failed-to-run-init-ucode-5

--- hardware/x230.nix
+++ hardware/x230.nix
@@ -3,6 +3,7 @@
 {
   boot.initrd.availableKernelModules = [ "xhci_pci" "ehci_pci" "ahci" "usb_storage" "sd_mod" "sdhci_pci" ];
   boot.initrd.kernelModules = [ ];
+  boot.blacklistedKernelModules = [ "intel_ips" ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
