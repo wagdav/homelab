750414556ca5ead97c64d02058a9aafe7ed982c6
David Wagner <wagdav@gmail.com>
2021-03-09 21:27:50 +0100

    loki: add an explicit local configuration
    Source: https://grafana.com/docs/loki/latest/configuration/examples/#complete-local-config

--- modules/loki.nix
+++ modules/loki.nix
@@ -1,7 +1,52 @@
 { config, pkgs, ... }:
 let
-
   httpPort = 3100;
+
+  # https://grafana.com/docs/loki/latest/configuration/examples/#complete-local-config
+  configuration = {
+    auth_enabled = false;
+
+    server.http_listen_port = httpPort;
+
+    ingester = {
+      lifecycler = {
+        address = "127.0.0.1";
+        ring = {
+          kvstore.store = "inmemory";
+          replication_factor = 1;
+        };
+        final_sleep = "0s";
+      };
+      chunk_idle_period = "5m";
+      chunk_retain_period = "30s";
+    };
+
+    schema_config = {
+      configs = [
+        {
+          from = "2020-05-15";
+          store = "boltdb";
+          object_store = "filesystem";
+          schema = "v11";
+          index = {
+            prefix = "index_";
+            period = "168h";
+          };
+        }
+      ];
+    };
+
+    storage_config = {
+      boltdb.directory = "/tmp/loki/index";
+      filesystem.directory = "/tmp/loki/chunks";
+    };
+
+    limits_config = {
+      enforce_metric_name = false;
+      reject_old_samples = true;
+      reject_old_samples_max_age = "168h";
+    };
+  };
 
 in
 {
@@ -9,9 +54,7 @@ in
 
   services.loki = {
     enable = true;
-    configFile = ''
-      ${pkgs.grafana-loki.src}/cmd/loki/loki-local-config.yaml
-    '';
+    inherit configuration;
   };
 
   services.consul.catalog = [
