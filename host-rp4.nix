807bb7dfd0c48dad3bdf71c582b73ca9154a4bbe
David Wagner <wagdav@gmail.com>
2021-10-21 08:14:25 +0200

    Kodi: add packages

--- host-rp4.nix
+++ host-rp4.nix
@@ -13,10 +13,14 @@
   # KODI
   services.xserver.enable = true;
   services.xserver.desktopManager.kodi.enable = true;
+  services.xserver.desktopManager.kodi.package = pkgs.kodi.withPackages (p: with p; [ kodi-platform youtube ]);
   services.xserver.displayManager.autoLogin.enable = true;
   services.xserver.displayManager.autoLogin.user = "kodi";
 
-  users.extraUsers.kodi.isNormalUser = true;
+  users.users.kodi = {
+    isNormalUser = true;
+    extraGroups = [ "video" ];
+  };
 
   networking.firewall = {
     allowedTCPPorts = [ 8080 ];
@@ -29,5 +33,15 @@
       port = 8080;
       tags = (import modules/lib/traefik.nix).tagsForHost "tv";
     }
+  ];
+
+  nixpkgs.overlays = [
+    (self: super: {
+      kodi = super.kodi.override {
+        sambaSupport = false;
+        rtmpSupport = false;
+        joystickSupport = false;
+      };
+    })
   ];
 }
