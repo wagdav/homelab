79e1eeefc87ad20fed37d6150329426738f40d15
David Wagner <wagdav@gmail.com>
2022-10-19 20:48:44 +0200

    Fix warning on strict reverse path filtering

--- modules/vpn.nix
+++ modules/vpn.nix
@@ -2,5 +2,9 @@
 
 {
   services.tailscale.enable = true;
+
+  # Strict reverse path filtering breaks Tailscale exit node use and some
+  # subnet routing setups.
+  networking.firewall.checkReversePath = "loose";
 }
 
