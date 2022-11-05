2bb03d36f798225b0a28584a16476fe0ea1c86e7
David Wagner <wagdav@gmail.com>
2022-11-05 18:03:46 +0100

    x230: bump state version

--- x230.nix
+++ x230.nix
@@ -185,5 +185,5 @@
   # compatible, in order to avoid breaking some software such as database
   # servers. You should change this only after NixOS release notes say you
   # should.
-  system.stateVersion = "20.03"; # Did you read the comment?
+  system.stateVersion = "22.05"; # Did you read the comment?
 }
