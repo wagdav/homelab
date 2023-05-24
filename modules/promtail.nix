ac07d569279355c46512431ba3002eba8acdd186
David Wagner <wagdav@gmail.com>
2023-05-24 22:15:44 +0200

    promtail: switch to the official NixOS module

--- modules/promtail.nix
+++ modules/promtail.nix
@@ -1,56 +1,48 @@
 { config, pkgs, ... }:
 let
+
   httpPort = 9080;
-
-  configFile = pkgs.writeText "promtail-config.yaml" ''
-    server:
-      http_listen_port: ${toString httpPort}
-      grpc_listen_port: 0
-
-    positions:
-      filename: /tmp/positions.yaml
-
-    clients:
-      - url: http://loki.thewagner.home/loki/api/v1/push
-
-    scrape_configs:
-    - job_name: journal
-      journal:
-        path: /var/log/journal
-        max_age: 12h
-        labels:
-          job: systemd-journal
-      relabel_configs:
-       - source_labels: ['__journal__systemd_unit' ]
-         regex: '(.*)\.service'
-         target_label: 'service'
-       - source_labels: ['__journal__hostname']
-         target_label: 'hostname'
-  '';
 
 in
 {
   imports = [ ./consul-catalog.nix ];
 
-  systemd.services.promtail = {
-    description = "Promtail service for Loki";
-    wantedBy = [ "multi-user.target" ];
+  services.promtail = {
+    enable = true;
+    configuration = {
+      server.http_listen_port = httpPort;
+      server.grpc_listen_port = 0;
+      clients = [
+        {
+          url = "http://loki.thewagner.home/loki/api/v1/push";
+        }
+      ];
 
-    serviceConfig = {
-      ExecStart = ''
-        ${pkgs.grafana-loki}/bin/promtail --config.file ${configFile}
-      '';
-      User = "promtail";
-      Group = "promtail";
+      scrape_configs = [
+        {
+          job_name = "journal";
+          journal = {
+            path = "/var/log/journal";
+            max_age = "12h";
+            labels = {
+              job = "systemd-journal";
+            };
+          };
+          relabel_configs =
+            [
+              {
+                source_labels = [ "__journal__systemd_unit" ];
+                regex = "(.*)\\.service";
+                target_label = "service";
+              }
+              {
+                source_labels = [ "__journal__hostname" ];
+                target_label = "hostname";
+              }
+            ];
+        }
+      ];
     };
-  };
-
-  users.groups.promtail = { };
-  users.users.promtail = {
-    description = "Promtail Service User";
-    group = "promtail";
-    extraGroups = [ "systemd-journal" ];
-    isSystemUser = true;
   };
 
   services.consul.catalog = [
