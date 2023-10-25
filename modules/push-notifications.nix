e55608197598f4a0f42309e47be502e5d88a62af
David Wagner <wagdav@gmail.com>
2023-10-25 22:37:16 +0200

    Add cron job to periodically report room humidity

--- modules/push-notifications.nix
+++ modules/push-notifications.nix
@@ -9,4 +9,20 @@
   };
 
   networking.firewall.allowedTCPPorts = [ 8080 ];
+
+  users.users = {
+    ntfy = {
+      isSystemUser = true;
+      group = "ntfy";
+    };
+  };
+
+  users.groups."ntfy" = { };
+
+  services.cron = {
+    enable = true;
+    systemCronJobs = [
+      "0 9,12,22 * * * ntfy . ${../scripts/push-room-humidity.sh} >> /tmp/cron-ntfy.log 2>&1"
+    ];
+  };
 }
