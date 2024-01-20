ee9041858d0e53a4d16bd0fe1077b2168017c3c9
David Wagner <wagdav@gmail.com>
2024-01-20 17:09:10 +0100

    rp3: generate new host key

--- modules/buildMachines.nix
+++ modules/buildMachines.nix
@@ -9,7 +9,7 @@
 
     rp3 = {
       hostNames = [ "rp3" "rp3.thewagner.home" ];
-      publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJgVezT5bpUOihQmNEV6XC/RsUtFDcd9vcLZ7cUKPp83";
+      publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB35lHMw8SSz1n/xFR5TEXEEMvHoKrc0gFJ1bgniDfPp";
     };
 
     rp4 = {
