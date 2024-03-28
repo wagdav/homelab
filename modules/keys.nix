23934fb3fa30a45ea4d0d6c8d2376a8d3dd54705
David Wagner <wagdav@gmail.com>
2024-03-28 17:17:51 +0100

    Update keys.nix
    Remove extra Homelab key

--- modules/keys.nix
+++ modules/keys.nix
@@ -1,7 +1,6 @@
 {
   dwagner = [
     "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDK3SuOKZRMotTbXMaArK+ano/QUKkttcn/+q1v4roBnOQXfPpG16P4NHrSA6SdIwCb2I70thiegCrPRDm0VyZ4VHICxwkmoznmsti6ZlQTvFeHvE22xxYOqPM9FzKGtHe/lPdqrVArzwAFlRpwRAT4TZxMDTGjSnVr3C+N5yBG5ACERStmqs+3phYN0ISYz5usVahhZqh3tKfjb4+UvXiNEC6qGXWpH2ZqT6sYgajmRAeB8Ke9+E1OTL32A0eP7Zri+aWU8+jg62MjH4zhq7bEwYXF7AhUVoi2skLTHv8W3u+7uWhtrSgWg2N7Z4ip6OPAcboCrEUQ+5/yFvhhs1hP wagdav@gmail.com"
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICubR6tJCBB1JvKdmJM2aTyHye3jpt0UOQZPqRlVOuac wagdav+homelab@gmail.com"
   ];
 }
 
