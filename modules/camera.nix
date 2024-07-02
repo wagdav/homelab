dc7bdece4ae0f659e77762de9d71d786be48b078
David Wagner <wagdav@gmail.com>
2024-07-02 11:18:00 +0200

    rp3: Remove mjpg streamer

--- modules/camera.nix
+++ modules/camera.nix
@@ -1,7 +1,3 @@
 { config, ... }:
 
-{
-  services.mjpg-streamer.enable = true;
-  services.mjpg-streamer.inputPlugin = "input_uvc.so -vf true";
-  networking.firewall.allowedTCPPorts = [ 5050 ];
-}
+{ }
