20996953c9c10d669b40e0c374fc198544890a2b
David Wagner <wagdav@gmail.com>
2024-08-20 20:50:37 +0200

    rp4: re-activate continous deployment

--- flake.nix
+++ flake.nix
@@ -81,7 +81,7 @@
             nuc = self.nixosConfigurations.nuc.config.system.build.toplevel;
             x230 = self.nixosConfigurations.x230.config.system.build.toplevel;
             rp3 = self.nixosConfigurations.rp3.config.system.build.toplevel;
-            # rp4 = self.nixosConfigurations.rp4.config.system.build.toplevel;
+            rp4 = self.nixosConfigurations.rp4.config.system.build.toplevel;
           };
         };
       };
