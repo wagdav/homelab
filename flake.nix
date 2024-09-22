fa0abfaf35b6a078f14c555818d752ae0f863342
David Wagner <wagdav@gmail.com>
2024-09-22 17:52:58 +0200

    x1: Enable continous deployment

--- flake.nix
+++ flake.nix
@@ -65,6 +65,7 @@
         cachix-deploy-spec = cachix-deploy-lib.spec {
           agents = {
             nuc = self.nixosConfigurations.nuc.config.system.build.toplevel;
+            x1 = self.nixosConfigurations.x1.config.system.build.toplevel;
             x230 = self.nixosConfigurations.x230.config.system.build.toplevel;
             rp3 = self.nixosConfigurations.rp3.config.system.build.toplevel;
             rp4 = self.nixosConfigurations.rp4.config.system.build.toplevel;
