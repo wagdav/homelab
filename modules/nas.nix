b9c5d9fc965d639e137d284e964bbc84d59ec12c
David Wagner <wagdav@gmail.com>
2024-12-08 12:44:28 +0100

    Use local name

--- modules/nas.nix
+++ modules/nas.nix
@@ -3,7 +3,7 @@
 {
   fileSystems = {
     "/mnt/nas" = {
-      device = "dns-320:/mnt/HD/HD_a2/Ajaxpf";
+      device = "dns-320.local:/mnt/HD/HD_a2/Ajaxpf";
       fsType = "nfs";
       options = [ "x-systemd.automount" "noauto" "_netdev" ];
     };
