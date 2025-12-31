7e4679ecf7f99befb8f5b4b10b8224566e5db317
David Wagner <wagdav@gmail.com>
2025-12-31 15:25:59 +0100

    fix(tailscale): Open firewall ports
    To help Tailscale directly connecting to the containers, let's open the
    relevant UDP ports.

--- modules/vpn.nix
+++ modules/vpn.nix
@@ -3,6 +3,7 @@
 {
   services.tailscale.enable = true;
   services.tailscale.interfaceName = if config.boot.isContainer then "userspace-networking" else "tailscale0";
-  services.resolved.enable = true;  # Recommended in https://github.com/tailscale/tailscale/issues/4254
+  services.tailscale.openFirewall = config.boot.isContainer;
+  services.resolved.enable = true; # Recommended in https://github.com/tailscale/tailscale/issues/4254
 }
 
