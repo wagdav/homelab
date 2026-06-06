02708510608203029cf6892c49f618cd4c6c8345
David Wagner <wagdav@gmail.com>
2026-06-06 21:53:20 +0200

    Add nixos-boot shim

---
+++ scripts/boot.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+set -e
+
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 HOST" >&2
+  exit 1
+fi
+
+host="$1"
+
+if [ "$host" = "$(hostname)" ]; then
+    sudo nixos-rebuild boot \
+        --flake ".#$host"
+else
+    nix run 'nixpkgs#nixos-rebuild' -- boot \
+        --flake ".#$host" \
+        --target-host "root@$host" \
+        --build-host  "root@$host" \
+        --fast
+fi
