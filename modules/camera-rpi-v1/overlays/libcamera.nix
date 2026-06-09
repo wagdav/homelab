fbcb412e0c7cd61e37d3940137a735203167d0a2
David Wagner <wagdav@gmail.com>
2026-06-09 21:49:01 +0200

    Update libcamera to v0.7.1+rpt20260609
    Remove the reduntant parts from the overlay.

--- modules/camera-rpi-v1/overlays/libcamera.nix
+++ modules/camera-rpi-v1/overlays/libcamera.nix
@@ -1,40 +1,20 @@
 final: prev: {
   libcamera = prev.libcamera.overrideAttrs (old: {
-    buildInputs = old.buildInputs ++ [ prev.boost prev.nlohmann_json ];
-    nativeBuildInputs = old.nativeBuildInputs ++ [ prev.python3Packages.pybind11 ];
-
-    BOOST_INCLUDEDIR = "${prev.lib.getDev prev.boost}/include";
-    BOOST_LIBRARYDIR = "${prev.lib.getLib prev.boost}/lib";
-
-    postPatch = old.postPatch + ''
-      patchShebangs src/py/libcamera
-    '';
+    buildInputs = old.buildInputs ++ [ prev.elfutils prev.libpisp prev.libtiff ];
 
     mesonFlags = old.mesonFlags ++ [
       "-Dcam=disabled"
       "-Dgstreamer=disabled"
       "-Dipas=rpi/vc4,rpi/pisp"
       "-Dpipelines=rpi/vc4,rpi/pisp"
+      "-Drpi-awb-nn=disabled"
     ];
 
     src = prev.fetchFromGitHub {
       owner = "raspberrypi";
       repo = "libcamera";
-      rev = "f0e40f1c50bd0afe65727d6e407d0dcb42666ada";
-      sha256 = "sha256-sJKzmeeXD/66P5o+X9w3J2gwxDNsdBUdXEqU6goJdN4=";
-
-      nativeBuildInputs = [ prev.git ];
-
-      postFetch = ''
-        cd "$out"
-
-        export NIX_SSL_CERT_FILE=${prev.cacert}/etc/ssl/certs/ca-bundle.crt
-
-        ${prev.lib.getExe prev.meson} subprojects download \
-          libpisp
-
-        find subprojects -type d -name .git -prune -execdir rm -r {} +
-      '';
+      rev = "v0.7.1+rpt20260609";
+      sha256 = "sha256-qSCaAW5WuHcdg+i6YVLEF0rj3uM0Usw+I+hjde7yWvQ=";
     };
   });
 }
